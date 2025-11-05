727c0816eaa10a389700f5beb29c438f57445e1b
Manuel Alejandro Cortez Velasquez <malejandro.cortez91@gmail.com>
2025-11-05 23:24:11 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve 'ne

--- src/navigation.ts
+++ src/navigation.ts
@@ -3,8 +3,8 @@ import {
   Pathnames,
 } from 'next-intl/navigation';
 
-export const locales = ['en', 'es'] as const;
-export const localePrefix = 'always';
+export const locales = ['en'] as const;
+export const localePrefix = 'as-needed';
 
 export const pathnames = {
   '/': '/',
