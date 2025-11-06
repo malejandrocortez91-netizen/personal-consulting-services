0f25074ae3ed6063c85c21a94339e177726d19f8
Manuel Alejandro Cortez Velasquez <malejandro.cortez91@gmail.com>
2025-11-06 00:31:07 +0000

    error while publishing this version

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,6 @@ import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
-  experimental: {
-    allowedDevOrigins: [
-      'https://*.googleusercontent.com',
-      'https://*.cloudworkstations.dev',
-    ],
-  },
   images: {
     remotePatterns: [
       {
