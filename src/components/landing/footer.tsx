7eee749d632cbfcf5fcae1c9492cc6f8ceec0a02
Manuel Alejandro Cortez Velasquez <malejandro.cortez91@gmail.com>
2025-11-07 05:57:39 +0000

    ADD THE SMOOTH SCROLL ANIMATION WHEN CLICKING THE NAME TO GET TO THE TOP

--- src/components/landing/footer.tsx
+++ src/components/landing/footer.tsx
@@ -15,7 +15,7 @@ export default function Footer() {
     <footer className="bg-card border-t">
       <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 py-8 md:h-24 md:flex-row">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <Link href="/" className="flex items-center space-x-3">
+          <Link href="/#hero" className="flex items-center space-x-3">
             <MemoryStick className="h-6 w-6 text-primary" />
             <span className="inline-block font-font-handwriting text-2xl font-bold text-primary">
               Alejandro Cortez Velasquez
--- src/components/landing/header.tsx
+++ src/components/landing/header.tsx
@@ -10,7 +10,7 @@ export default function Header() {
     <header className="sticky top-0 z-40 w-full border-b bg-card/80 backdrop-blur-sm">
       <div className="container mx-auto flex h-16 items-center justify-between space-x-4 px-4">
         <div className="flex gap-6 md:gap-10">
-          <Link href="/" className="flex items-center space-x-3">
+          <Link href="/#hero" className="flex items-center space-x-3">
             <MemoryStick className="h-6 w-6 text-primary" />
             <span className="inline-block font-font-handwriting text-2xl font-bold text-primary">
               Alejandro Cortez Velasquez
