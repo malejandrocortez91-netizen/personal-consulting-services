e4d27e6a627eb5e56e9c2296e2f3f59a862b6df8
Manuel Alejandro Cortez Velasquez <malejandro.cortez91@gmail.com>
2025-11-19 08:31:06 +0000

    you can speed up the sliding/transition? add a control so they can "scro

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -104,7 +104,7 @@ export default {
       animation: {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
-        'marquee-slow': 'marquee-slow 75s linear infinite',
+        'marquee-slow': 'marquee-slow 50s linear infinite',
       },
     },
   },
